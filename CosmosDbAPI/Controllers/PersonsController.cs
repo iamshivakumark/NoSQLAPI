1b5c45bb702ad5d5ebab5ed72a2ba74562007a56
Shiva Kumar K <shivakumarkotakondaskk@gmail.com>
2023-05-16 11:20:59 +0530

    updatedactionmethodnames

--- CosmosDbAPI/Controllers/PersonsController.cs
+++ CosmosDbAPI/Controllers/PersonsController.cs
@@ -16,34 +16,34 @@ namespace CosmosDbAPI.Controllers
         }
         // GET api/items
         [HttpGet]
-        public async Task<IActionResult> List()
+        public async Task<IActionResult> GetAll()
         {
             return Ok(await _cosmosDbService.GetAllAsync("SELECT * FROM c"));
         }
         // GET api/items/5
         [HttpGet("{id}/{partitionkey}")]
-        public async Task<IActionResult> Get(string id, string partitionkey)
+        public async Task<IActionResult> GetById(string id, string partitionkey)
         {
             return Ok(await _cosmosDbService.GetByIdAsync(id, partitionkey));
         }
         // POST api/items
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Persons person)
+        public async Task<IActionResult> CreatePerson([FromBody] Persons person)
         {
             person.PersonID = Guid.NewGuid().ToString();
             await _cosmosDbService.AddPersonAsync(person);
-            return CreatedAtAction(nameof(Get), new { id = person.PersonID, partitionkey = person.FirstName }, person);
+            return CreatedAtAction(nameof(GetById), new { id = person.PersonID, partitionkey = person.FirstName }, person);
         }
         // PUT api/items/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Edit([FromBody] Persons person)
+        public async Task<IActionResult> UpdatePerson([FromBody] Persons person)
         {
             await _cosmosDbService.UpdateByIdAsync(person.PersonID, person);
             return NoContent();
         }
         // DELETE api/items/5
         [HttpDelete("{id}/{partitionkey}")]
-        public async Task<IActionResult> Delete(string id, string partitionkey)
+        public async Task<IActionResult> DeletePersonById(string id, string partitionkey)
         {
             await _cosmosDbService.DeleteByIdAsync(id, partitionkey);
             return NoContent();
